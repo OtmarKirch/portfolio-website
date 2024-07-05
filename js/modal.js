0f69bd21cb0b18fea6c51924f1bf3b9c1f2baeff
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-07-05 21:06:52 +0200

    Add project mySciFiApp

---
+++ js/modal.js
@@ -0,0 +1,61 @@
+
+// HTML structure for the modal in projectData.js
+
+
+// CSS for the modal
+
+// Append the modal HTML and CSS to the document
+// document.head.insertAdjacentHTML("beforeend", modalCSS);
+// document.body.insertAdjacentHTML("beforeend", modalHTML);
+
+// Modal functionality
+const modal = document.getElementById("projectModal");
+//modal.innerHTML = mySciFiAppModalHTML;
+
+// When the user clicks on <span> (x), close the modal
+
+
+// Function to open the modal
+function openModal(project) {
+  if (project === "mySciFiApp") {
+   modal.innerHTML = mySciFiAppModalHTML;
+  
+  } 
+  if (project === "meetApp") {
+    modal.innerHTML = meetAppModalHTML;
+  };
+  modal.style.display = "block";
+  const close = document.getElementById("close");
+  close.onclick = function () {
+  modal.style.display = "none";
+};
+}
+
+// Listen for a click on the window
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// // Modal functionality
+// const meetAppModal = document.getElementById("meetAppModal");
+// meetAppModal.innerHTML = meetAppModalHTML;
+// const closeMeetApp = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// closeMeetApp.onclick = function () {
+//   meetAppModal.style.display = "none";
+// };
+
+// // Function to open the modal
+// function openMeetAppModal() {
+//   meetAppModal.style.display = "block";
+// }
+
+// // Listen for a click on the window
+// window.onclick = function (event) {
+//   if (event.target == meetAppModal) {
+//     meetAppModal.style.display = "none";
+//   }
+// };
---
+++ js/projectData.js
@@ -0,0 +1,40 @@
+const mySciFiAppModalHTML = `
+  <div class="modal-content">
+  <span id="close">&times;</span>
+    <h2>mySciFi App</h2>
+  <p> \u2192 Take a look at <a href="https://myscifiapp.netlify.app/">the app</a></p>
+  <p> \u2192 See the code and more information on my <a href="https://github.com/OtmarKirch/MySciFi-client">GitHub repository</a></p>
+  <p> \u2192 Take a look at <a href="https://quiet-bastion-19832-9b36523e0b42.herokuapp.com/documentation.html">the server</a></p>
+  <p> \u2192 See the code and more information on my <a href="https://github.com/OtmarKirch/sci-fi-app">GitHub repository</a></p>
+  
+            <img class="mySciFiApp--presentation" src="./img/projects/mySciFiApp/mySciFiApp1.png" alt="">
+            <img class="mySciFiApp--presentation" src="./img/projects/mySciFiApp/mySciFiApp2.png" alt="">
+            <img class="mySciFiApp--presentation" src="./img/projects/mySciFiApp/mySciFiApp3.png" alt="">
+            <img class="mySciFiApp--presentation" src="./img/projects/mySciFiApp/mySciFiApp4.png" alt="">
+            <img class="mySciFiApp--presentation" src="./img/projects/mySciFiApp/mySciFiApp5.png" alt="">
+            <img class="mySciFiApp--presentation" src="./img/projects/mySciFiApp/mySciFiApp6.png" alt="">
+            <img class="mySciFiApp--presentation" src="./img/projects/mySciFiApp/mySciFiApp7.png" alt="">
+            <img class="mySciFiApp--presentation" src="./img/projects/mySciFiApp/mySciFiApp8.png" alt="">
+  </div>
+`;
+
+const meetAppModalHTML = `
+  <div class="modal-content">
+  <span id="close">&times;</span>
+    <h2>Meet App</h2>
+  <p>This project involves building a serverless, progressive web application (PWA) using React and a test-driven development (TDD) approach. The application leverages the Google Calendar API to fetch upcoming events. By combining serverless and PWA concepts, the application benefits from no backend maintenance, scalability, availability, no idle time costs, instant loading, offline support, push notifications, "add to home screen" prompt, responsive design, and cross-platform compatibility. The TDD approach ensures high-quality code by writing tests before actual functionality. The application also includes data visualization through graphs, enhancing its visual appeal and data interpretation. Users can search for a city and get a list of events hosted in that city. The data visualization component includes a scatter plot showing the number of events in each location and a pie chart visualizing the popularity of event genres.</p>
+  <p> \u2192 Take a look at <a href="https://otmarkirch.github.io/meetApp/">the app</a></p>
+  <p> \u2192 See the code and more information on my <a href="https://github.com/OtmarKirch/meetApp">GitHub repository</a></p>
+    <h3>Technologies</h3>
+    <p>React, Google Calendar API, AWS Lambda, AWS API Gateway, AWS S3, AWS CloudFront, Jest, Puppeteer, Recharts, HTML, CSS</p>
+    
+    <h3>Impressions</h3>
+    <p>The meet app at the start. It shows 32 events of all cities by default with their statistics.</p>
+            <img id="img_app" src="./img/projects/meetApp/meetApp.png" alt="meet app impression">
+            <p>The input allows for searching and selecting cities.</p>
+            <img id="img_citySearch" src="./img/projects/meetApp/meetApp_citySearch.png" alt="meet app impression">
+            <p>Alerts are shown when the app is offline or when the user types incorrect input.</p>
+            <img id="img_alert" src="./img/projects/meetApp/meetApp_alerts.png" alt="meet app impression">
+            
+  </div>
+`;
